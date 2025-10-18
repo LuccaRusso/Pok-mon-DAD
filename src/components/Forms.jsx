e7000f7d0ba85176bfbbecc5420be4657aebfb8d
DudaCacciatore <mdoriacacciatore@gmail.com>
2025-10-18 18:35:22 -0300

    add lista de pokemons cadastrados

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -1,41 +1,42 @@
 import styles from "./Forms.module.css";
 import { useState } from "react";
+import List from "./List";
 
 function Forms() {
-
     const [mensagem, setMensagem] = useState(null);
     const [tipoMensagem, setTipoMensagem] = useState("");
+    const [listPokemons, setListPokemons] = useState([]);
 
     const handleSubmit = (e) => {
-        e.preventDefault(); //não envia as informações do forms
+        e.preventDefault();
     
         const form = e.target;
         const nome = form.nome.value.trim();
         const tipo = form.tipo.value;
         const descricao = form.descricao.value.trim();
         const poder = form.poder.value;
-    
-        if (!nome || !tipo || !descricao || !poder) {
+
+        if (!nome || !tipo || !descricao || poder === "") {
             setTipoMensagem("erro");
             setMensagem("Preencha todos os campos!");
-          } else {
+        } else {
+            const novoPokemon = { nome, tipo, descricao, poder };
+            setListPokemons([...listPokemons, novoPokemon]);
+
             setTipoMensagem("sucesso");
             setMensagem("Pokémon cadastrado!");
             form.reset();
-          }
-      };
+        }
+    };
 
     return (
         <>
-            <form className={styles.forms} onSubmit={handleSubmit} method="post" >
+            <form className={styles.forms} onSubmit={handleSubmit} method="post">
                 <label htmlFor="nome">Nome do Pokémon</label>
-                <input id="nome"/>
+                <input id="nome" name="nome" />
 
                 <label htmlFor="tipo">Tipo</label>
-                <select
-                    id="tipo"
-                    name="tipo"
-                >
+                <select id="tipo" name="tipo">
                     <option value="" disabled>Selecione o tipo</option>
                     <option value="1">🔥 fogo</option>
                     <option value="2">💧 água</option>
@@ -49,19 +50,20 @@ function Forms() {
                 <textarea name="descricao"></textarea>
 
                 <label htmlFor="poder">Poder</label>
-                <input type="number" name="poder" placeholder="8" min="0" max="100"/>
+                <input type="number" name="poder" placeholder="8" min="0" max="100" />
 
                 <button type="submit">Cadastrar Pokémon</button>
             </form>
 
             {mensagem && (
-                <p className={
-                    tipoMensagem === "erro" ? styles.mensagemErro : styles.mensagemSucesso}>
-                {mensagem}
+                <p className={tipoMensagem === "erro" ? styles.mensagemErro : styles.mensagemSucesso}>
+                    {mensagem}
                 </p>
             )}
+
+            <List listPokemons={listPokemons} />
         </>
-    )
+    );
 }
 
-export default Forms
+export default Forms;
